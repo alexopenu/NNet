a9e5a551792c7108054b188e9d915d4872a31f47
alexus <alexus.openu@gmail.com>
2019-11-13 12:02:52 -0800

    added the feature of splitting a net into two

--- python/nnet.py
+++ python/nnet.py
@@ -241,6 +241,94 @@ class NNet():
                 outputs[i,j] = outputs[i,j]*self.ranges[-1]+self.means[-1]
         return outputs.T
 
+    def evaluate_network_nonorm(self, inputs):
+
+        '''
+        Evaluate network using given inputs, without normalizing the inputs
+
+        Args:
+            inputs (numpy array of floats): Network inputs to be evaluated
+
+        Returns:
+            (numpy array of floats): Network output
+        '''
+        numLayers = self.numLayers
+        inputSize = self.inputSize
+        outputSize = self.outputSize
+        biases = self.biases
+        weights = self.weights
+
+        # # Prepare the inputs to the neural network
+        # inputsNorm = np.zeros(inputSize)
+        # for i in range(inputSize):
+        #     if inputs[i] < self.mins[i]:
+        #         inputsNorm[i] = (self.mins[i] - self.means[i]) / self.ranges[i]
+        #     elif inputs[i] > self.maxes[i]:
+        #         inputsNorm[i] = (self.maxes[i] - self.means[i]) / self.ranges[i]
+        #     else:
+        #         inputsNorm[i] = (inputs[i] - self.means[i]) / self.ranges[i]
+
+
+        #No normalization
+        inputsNorm = inputs
+
+        # Evaluate the neural network
+        for layer in range(numLayers - 1):
+            inputsNorm = np.maximum(np.dot(weights[layer], inputsNorm) + biases[layer], 0)
+        outputs = np.maximum(np.dot(weights[-1], inputsNorm) + biases[-1],0)
+
+        # # Undo output normalization
+        # for i in range(outputSize):
+        #     outputs[i] = outputs[i] * self.ranges[-1] + self.means[-1]
+        return outputs
+
+    def evaluate_network_multiple_nonorm(self, inputs):
+        '''
+        Evaluate network using multiple sets of inputs without normalization
+
+        Args:
+            inputs (numpy array of floats): Array of network inputs to be evaluated.
+
+        Returns:
+            (numpy array of floats): Network outputs for each set of inputs
+        '''
+
+        numLayers = self.numLayers
+        inputSize = self.inputSize
+        outputSize = self.outputSize
+        biases = self.biases
+        weights = self.weights
+        inputs = np.array(inputs).T
+
+        # Prepare the inputs to the neural network
+        numInputs = inputs.shape[1]
+        # inputsNorm = np.zeros((inputSize, numInputs))
+        # for i in range(inputSize):
+        #     for j in range(numInputs):
+        #         if inputs[i, j] < self.mins[i]:
+        #             inputsNorm[i, j] = (self.mins[i] - self.means[i]) / self.ranges[i]
+        #         elif inputs[i, j] > self.maxes[i]:
+        #             inputsNorm[i, j] = (self.maxes[i] - self.means[i]) / self.ranges[i]
+        #         else:
+        #             inputsNorm[i, j] = (inputs[i, j] - self.means[i]) / self.ranges[i]
+
+
+
+        inputsNorm = inputs
+
+        # Evaluate the neural network
+        for layer in range(numLayers - 1):
+            inputsNorm = np.maximum(np.dot(weights[layer], inputsNorm) + biases[layer].reshape((len(biases[layer]), 1)),
+                                    0)
+        outputs = np.dot(weights[-1], inputsNorm) + biases[-1].reshape((len(biases[-1]), 1))
+
+        # # Undo output normalization
+        # for i in range(outputSize):
+        #     for j in range(numInputs):
+        #         outputs[i, j] = outputs[i, j] * self.ranges[-1] + self.means[-1]
+
+        return outputs.T
+
     def num_inputs(self):
         ''' Get network input size'''
         return self.inputSize
--- test/TestNnetExtensions.py
+++ test/TestNnetExtensions.py
@@ -16,8 +16,8 @@ fileTarget2 = '../nnet/TestNetwork_output2.nnet'
 
 (weights, biases, inputMins, inputMaxes, means, ranges) = readNNet(fileOrigin,True)
 
-nnet2 = NNet(weights, biases, inputMins, inputMaxes, means, ranges)
-nnet2.write_to_file(fileTarget)
+nnet = NNet(weights, biases, inputMins, inputMaxes, means, ranges)
+nnet.write_to_file(fileTarget)
 
 
 print(filecmp.cmp(fileOrigin,fileTarget)) #True
@@ -25,22 +25,73 @@ print(filecmp.cmp(fileOrigin,fileTarget)) #True
 writeNNet(weights,biases,inputMins,inputMaxes,means,ranges,fileTarget2)
 print(filecmp.cmp(fileOrigin,fileTarget2)) #True
 
-writeNNet(nnet2.weights,nnet2.biases,nnet2.mins,nnet2.maxes,nnet2.means,nnet2.ranges,fileTarget2)
+writeNNet(nnet.weights,nnet.biases,nnet.mins,nnet.maxes,nnet.means,nnet.ranges,fileTarget2)
 print(filecmp.cmp(fileOrigin,fileTarget2)) #True
 
+'''
 print nnet2.numLayers
 x = splitNNet(nnet2,5)
 print x
-
-
 '''
+
+
 nnet = NNet.fromfilename(fileOrigin)
 #nnet.write_to_file(fileTarget2)
+'''
 writeNNet(nnet.weights, nnet.biases, nnet.mins, nnet.maxes, nnet.means, nnet.ranges, fileTarget2) #Error!
 print(filecmp.cmp(fileOrigin,fileTarget2))
 '''
 
+half = nnet.numLayers/2
 
+
+list = [1,2,3,4]
+
+list1,list2 = splitList(list,2)
+print(list1)
+print(list2)
+
+nnet_1,nnet_2 = splitNNet(nnet,half)
+
+print nnet.numLayers
+print nnet_1.numLayers
+print nnet_2.numLayers
+
+
+
+outputs=nnet.evaluate_network_nonorm([15299.0, 0.0, -3.1, 600.0, 500.0])
+print(outputs)
+
+
+outputs1 = nnet_1.evaluate_network_nonorm([15299.0, 0.0, -3.1, 600.0, 500.0])
+print(len(outputs1))
+#print(outputs1)
+outputs2 = nnet_2.evaluate_network_nonorm(outputs1)
+print(outputs2)
+
+print(outputs == outputs2)
+
+'''
+print(len(outputs) == len(outputs2))
+print len(outputs)
+
+x = [1.0,2.0]
+y = [1.0,4.0]
+
+print x==y
+
+x = [1,2]
+y = [1,2]
+print x==y
+y.append(3)
+print(y)
+print(x)
+print x==y
+y=x.append(3)
+print(y)
+print(x)
+print x==y
+'''
 
 
 
